a8f6f719e405e2c620c6722f978d4e995c2e7c72
Hadiul Islam <hadiulislamofficial@gmail.com>
2023-06-24 16:00:27 +0000

    updated time and space complexiti

--- math_algo/01_fibonacci.js
+++ math_algo/01_fibonacci.js
@@ -7,11 +7,11 @@
 
 function fibonacci(n){
     const fib = [0,1]
-    for(let i = 2; i<n; i++){ //time: O(n), space: O(1)
+    for(let i = 2; i<n; i++){ 
         fib[i] = fib[i-1] + fib[i-2]
       }
     return fib
-}
+}//time: O(n), space: O(1)
 
 console.log(fibonacci(2));
 console.log(fibonacci(3));
